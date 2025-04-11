24864b672cd21eb908df9044a4b1581312210840
Yamina Boukari <87201452+YaminaB@users.noreply.github.com>
2025-04-11 14:12:28 +0000

    started generating dummy data in python

---
+++ analysis/generate_dummy_data.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+import numpy as np
+
+# create an skeleton df
+dummy_data = pd.DataFrame({'id': range(1, 1001)})
+
+print(dummy_data)
+
+# generate ethnicities variable based on OpenSAFELY ethnicity short data report paper 
+
+ethnicities
+
+
+# generate column indicating migrant status
+
