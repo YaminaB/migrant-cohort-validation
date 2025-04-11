203dadf3d2596ddcd9f560f5d9124a61e4e2f450
Yamina Boukari <87201452+YaminaB@users.noreply.github.com>
2025-04-11 11:35:15 +0000

    Added remaining variables to study definition

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,19 +1,22 @@
 # This is a script to create the following migrant cohort with basic demographics:
-# - Select anyone with a migration-related code at any time point during the study period AND who was registered at anytime (2009-2024)
+# - Select anyone with 
+#         1) a migration-related code at any time point during the study period AND 
+#         2) who was registered at anytime (2009-2024) AND
+#         3) who does not have a disclosive sex 
 # - Add a variable to indicate the date when they got their first migration code 
 # - Add a variable to indicate number of migration codes in their record
 # - Add a variable to indicate the category of migration-related code 
+# - Add a sex variable
 # - Add date of first practice registration variable
 # - Add time from first practice registration to first migration code
 # - Add ethnicity variable (using SNOMED:2022 codelist)
 # - Add year of birth variable
 # - Add practice region variable (or equivalent)
 # - Add IMD variable 
-# - Add sex variable
 # - Add a date of death (if there is one)
 
 from ehrql import create_dataset, codelist_from_csv, show
-from ehrql.tables.tpp import patients, practice_registrations, clinical_events
+from ehrql.tables.tpp import addresses, patients, practice_registrations, clinical_events, ons_deaths
 
 # Load codelists 
 
@@ -44,7 +47,10 @@ ethnicity_codelist = codelist_from_csv(
 study_start_date = "2009-01-01"
 study_end_date = "2024-12-31"
 
-# Select all individuals who 1) had a migrant code during the entire study period AND were registered at some point during the period
+# Select all individuals who:
+#          1) had a migrant code during the entire study period AND 
+#          2) were registered at some point during the period AND 
+#          3) has a non-disclosive sex
 # Add a variable indicating the date of the first code 
 # Add a variable indicating how many migration-related codes they have
 
@@ -58,18 +64,26 @@ is_registered_during_study = (
     .exists_for_patient()
 )           
 
-dataset = create_dataset()
-dataset.define_population(has_any_migrant_code & is_registered_during_study)
+has_non_disclosive_sex = (
+    (patients.sex == "male") | (patients.sex == "female")
+)
 
-dataset.date_of_first_migration_code = (
+dataset = create_dataset()
+dataset.define_population(has_any_migrant_code & is_registered_during_study & has_non_disclosive_sex)
+
+date_of_first_migration_code = (
     clinical_events.where(clinical_events.snomedct_code.is_in(all_migrant_codes))
     .sort_by(clinical_events.date)
-    .first_for_patient().date
-)
+    .first_for_patient().date)
+
+dataset.date_of_first_migration_code = date_of_first_migration_code
+
 dataset.number_of_migration_codes = (
     clinical_events.where(clinical_events.snomedct_code.is_in(all_migrant_codes))
     .count_for_patient()
 )
+
+dataset.sex = patients.sex
 
 # Add variables to indicate the type of migration code
 
@@ -79,33 +93,23 @@ dataset.has_interpreter_migrant_code = clinical_events.where(clinical_events.sno
 
 # Add first practice registration date
 
-dataset.first_practice_registration_date = (
+date_of_first_practice_registration = (
     practice_registrations.sort_by(practice_registrations.start_date)
     .first_for_patient().start_date
 )
 
-show(dataset)
+dataset.date_of_first_practice_registration = date_of_first_practice_registration
 
-show(
-    clinical_events.where(clinical_events.snomedct_code.is_in(ethnicity_codelist))
-    .where(clinical_events.date.is_on_or_before("2023-01-01"))
-    .sort_by(clinical_events.date)
-    .last_for_patient()
-    .snomedct_code
-)
+# Calculate time from first registration to first migration code 
 
-dataset.define_population(has_migrant_code)
+time_to_first_migration_code  = (date_of_first_migration_code - date_of_first_practice_registration).days
 
-dataset.sex = patients.sex
-dataset.first_migrant_code = clinical_events.where(clinical_events.snomedct_code.is_in(migrant_codes)).sort_by(clinical_events.date).first_for_patient().snomedct_code
-dataset.date_of_first_migrant_code = clinical_events.where(clinical_events.snomedct_code.is_in(migrant_codes)).sort_by(clinical_events.date).first_for_patient().date
-dataset.year_of_birth = patients.date_of_birth.year
-dataset.date_of_first_practice_registration = practice_registrations.sort_by(practice_registrations.start_date).first_for_patient().start_date
+dataset.time_to_first_migration_code = time_to_first_migration_code
 
+# Add ethnicity variable
 
 dataset.latest_ethnicity_code = (
     clinical_events.where(clinical_events.snomedct_code.is_in(ethnicity_codelist))
-    .where(clinical_events.date.is_on_or_before("2023-01-01"))
     .sort_by(clinical_events.date)
     .last_for_patient()
     .snomedct_code
@@ -114,7 +118,30 @@ dataset.latest_ethnicity_group = dataset.latest_ethnicity_code.to_category(
     ethnicity_codelist
 )
 
+# Add year of birth variable
+
+dataset.year_of_birth = (patients.date_of_birth).year
+
+# Add MSOA 
+
+address = addresses.for_patient_on("2021-03-21") # 2021 Census day
+
+dataset.msoa_code = address.msoa_code
+
+# Add IMD based on patient's address 
+
+dataset.imd_decile = address.imd_decile
+dataset.imd_quintile = address.imd_quintile
+
+# Add date of death (if died)
+
+dataset.TPP_death_date = patients.date_of_death
+dataset.ons_death_date = ons_deaths.date
+
+
+
+
 show(dataset)
 
-show(clinical_events.where(clinical_events.snomedct_code.is_in(ethnicity_codelist))
-     .snomedct_code)
+
+
